1be19cf0b8fafa0bfd83ce1eb1265f92eb3c29f4
Raf Swiderski <rafswid@icloud.com>
2021-03-10 10:58:40 +0000

    increase and decrease temp passing tests :)

--- jasmine/jasmine-standalone-3.6.0/spec/ThermostatSpec.js
+++ jasmine/jasmine-standalone-3.6.0/spec/ThermostatSpec.js
@@ -9,6 +9,17 @@ describe('Thermostat', function() {
   });
 
   it('starts at 20 degrees', function() {
-    expect(thermostat.temperature).toEqual(20);
+    expect(thermostat.getCurrentTemperature()).toEqual(20);
   });
+  
+  it('can be turned up by X degrees', function() {
+    thermostat.increaseTemp();
+    expect(thermostat.getCurrentTemperature()).toEqual(21);
+  });
+
+  it('can be turned down by X degrees', function() {
+    thermostat.decreaseTemp();
+    expect(thermostat.getCurrentTemperature()).toEqual(19);
+  });
+
 });
--- jasmine/jasmine-standalone-3.6.0/src/thermostat.js
+++ jasmine/jasmine-standalone-3.6.0/src/thermostat.js
@@ -2,4 +2,15 @@ class Thermostat {
     constructor() {
         this.temperature = 20;
     }
-}
+    getCurrentTemperature() {
+        return this.temperature;
+    }
+    increaseTemp() {
+        this.temperature += 1;
+        return this.temperature;
+    }
+    decreaseTemp() {
+        this.temperature -= 1;
+        return this.temperature;
+    }
+};
